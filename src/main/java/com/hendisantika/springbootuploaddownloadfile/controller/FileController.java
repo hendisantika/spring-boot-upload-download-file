d286a39110339f3fc7a17e94eaf2529ce821131f
hendisantika <hendisantika@yahoo.co.id>
2023-09-29 18:09:29 +0700

    #7 - Add singleFileUpload in FileController ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springbootuploaddownloadfile/controller/FileController.java
+++ src/main/java/com/hendisantika/springbootuploaddownloadfile/controller/FileController.java
@@ -3,8 +3,15 @@ package com.hendisantika.springbootuploaddownloadfile.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -27,6 +34,38 @@ public class FileController {
 
     @GetMapping("/")
     public String index(Model model) {
+        List<String> list = new ArrayList<String>();
+        File files = new File(UPLOADED_FOLDER);
+        String[] fileList = files.list();
+        Collections.addAll(list, fileList);
+        model.addAttribute("list", list);
+        return "index";
+    }
+
+    @PostMapping("/upload")
+    public String singleFileUpload(@RequestParam("file") MultipartFile file, Model model) {
+        if (file.isEmpty()) {
+            model.addAttribute("warning",
+                    "Please select a file to upload");
+            return "upload";
+        }
+
+        try {
+
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER +
+                    file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            model.addAttribute("message",
+                    "You successfully uploaded '"
+                            + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            model.addAttribute("error", "Error");
+            return "upload";
+        }
+
         List<String> list = new ArrayList<String>();
         File files = new File(UPLOADED_FOLDER);
         String[] fileList = files.list();
