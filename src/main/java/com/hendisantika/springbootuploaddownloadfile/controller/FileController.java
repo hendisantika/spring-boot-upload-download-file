82ac624dc106b8170ed103309c0b72e5859a47dd
hendisantika <hendisantika@yahoo.co.id>
2023-09-29 18:08:57 +0700

    #6 - Add index in FileController ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springbootuploaddownloadfile/controller/FileController.java
+++ src/main/java/com/hendisantika/springbootuploaddownloadfile/controller/FileController.java
@@ -1,6 +1,13 @@
 package com.hendisantika.springbootuploaddownloadfile.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,5 +23,15 @@ import org.springframework.stereotype.Controller;
 public class FileController {
 
     // Save the uploaded file to this folder
-    private static final String UPLOADED_FOLDER = "/home/user/Desktop/files/";
+    private static final String UPLOADED_FOLDER = "/home/hendisantika/Desktop/files/";
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<String> list = new ArrayList<String>();
+        File files = new File(UPLOADED_FOLDER);
+        String[] fileList = files.list();
+        Collections.addAll(list, fileList);
+        model.addAttribute("list", list);
+        return "index";
+    }
 }
