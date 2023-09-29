93cc34688f38ff9fd159d9204a69fec0266baa6f
hendisantika <hendisantika@yahoo.co.id>
2023-09-29 18:08:08 +0700

    #5 - Add FileController ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootuploaddownloadfile/controller/FileController.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootuploaddownloadfile.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-upload-download-file
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/29/23
+ * Time: 18:07
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class FileController {
+
+    // Save the uploaded file to this folder
+    private static final String UPLOADED_FOLDER = "/home/user/Desktop/files/";
+}
