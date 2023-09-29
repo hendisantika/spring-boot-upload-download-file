ea8b4e327c324c6053dab69d4f9463a6f77c93a2
hendisantika <hendisantika@yahoo.co.id>
2023-09-29 18:04:37 +0700

    #1 - Initial commit ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootuploaddownloadfile/SpringBootUploadDownloadFileApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootuploaddownloadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadDownloadFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUploadDownloadFileApplication.class, args);
+    }
+
+}
